e951f72992595950cf0c44f84a9ba8f275596cdf
Мохаммад Дидаев <mohammaddidaev@MacBook-Air-Mohammad.local>
2023-11-04 10:56:27 +0300

    asd

--- routes/event.route.js
+++ routes/event.route.js
@@ -5,4 +5,4 @@ const {eventController} = require('../controllers/event.controller')
 router.get('/event', eventController.getEvent)
 router.post('/addEvent', eventController.addEvent)
 
-module.exports = router
+module.exports = router;
